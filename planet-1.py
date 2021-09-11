435e49a00c6af70821490ad2adb45d7bed1bf338
Anna Dvoskina <anna.dvoskina@yandex.ru>
2021-09-11 17:22:12 +0300

    planet_test

--- planet-1.py
+++ planet-1.py
@@ -3,9 +3,10 @@ import ephem
 from datetime import datetime
 current_date = (datetime.date(datetime.now()))
 
-user_text = input(' input ')
-planet = getattr(ephem, user_text)()
-planet.compute(ephem.Date(current_date))
-const = ephem.constellation(planet)
-print(const)
+# user_text = input(' input ')
+# planet = getattr(ephem, user_text)()
+# planet.compute(ephem.Date(current_date))
+# const = ephem.constellation(planet)
+# print(const)
+print(ephem.next_full_moon(current_date))
 
